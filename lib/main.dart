dc2555d3348159c107ab7a130e01d40586eb6542
AbMamun12 <abdullah.mamun8191@gmail.com>
2024-08-08 13:49:38 +0600

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(IntroApp());
+runApp(IntroApp());
+
 }
-class IntroApp extends StatelessWidget{  // যেকোনো class কে widget এ convert করা যায়
+
+class IntroApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-return     MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.blueGrey,
-      title: Text('Home'),
-    ),
-    body:  Center(
-      child: Text('Hello World From Abdullah. Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common. On the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal Bengal tiger.',
-        textAlign: TextAlign.justify,
-        maxLines: 3,
-        style: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.w600,
-        decoration: TextDecoration.none,
-        letterSpacing: 1,
-        wordSpacing: 5,
-          overflow: TextOverflow.ellipsis,
-      ),),
-    ),
-  ),
-);
+  return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey,
+              title: Text('Home Button'),
+
+            ),
+            body:  Center(
+              child: Text('Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common. On the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal Bengal tiger. ',
+                textAlign: TextAlign.justify,
+                maxLines: 2,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  letterSpacing: 2,
+                  wordSpacing: 5,
+                  overflow: TextOverflow.ellipsis,
+              ),),
+            ),
+          )
+      );
+
   }
 
 }
